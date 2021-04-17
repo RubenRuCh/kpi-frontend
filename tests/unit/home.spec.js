1183f6b2fee48b859a3be2fffacceb9db997fefb
Rubén Rüger <ruben_27895@hotmail.com>
2021-04-17 14:32:42 +0200

    Initial unit test

---
+++ tests/unit/home.spec.js
@@ -0,0 +1,21 @@
+import { shallowMount } from '@vue/test-utils';
+import Home from '@/views/Home.vue';
+
+describe('Home.vue', () => {
+  it('Renders Home page', () => {
+    // Arrange
+    const wrapper = shallowMount(Home);
+
+    // Assert
+    expect(wrapper.text()).toMatch('Gestión de indicadores');
+  });
+
+  it('Contains cards to travel through app views', () => {
+    // Arrange
+    const wrapper = shallowMount(Home);
+    const card = wrapper.findComponent('el-card');
+
+    // Assert
+    expect(card.exists()).toBe(true);
+  });
+});
