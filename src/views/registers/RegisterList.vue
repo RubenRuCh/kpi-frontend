a85ef1ba1a719138785901962bdc4577e58583bb
rauloriol <eloriol@gmail.com>
2021-04-23 20:15:04 +0200

    ADD Vista de tabla con element ui

--- src/views/registers/RegisterList.vue
+++ src/views/registers/RegisterList.vue
@@ -14,14 +14,11 @@
     <h1>Registros del indicador {{id}} </h1>
 
     <h2>Datos KPI</h2>
-    <div v-if="selectedKpi">
-      <div>
-        {{ selectedKpi.title }}
-        {{ selectedKpi.descripcion }}
-      </div>
-    </div>
-
-    <hr/>
+      <kpi-item
+        v-if="selectedKpi"
+        :id="selectedKpi.id"
+        :key="selectedKpi.id"
+      ></kpi-item>
 
     <h2>Insertar registro</h2>
     <register-form :id="id" :columns="fillableFields" @save-data="saveData"></register-form>
@@ -29,20 +26,26 @@
     <hr />
 
     <h2>Registros</h2>
-    <table>
-      <tr v-for="register in registers" :key="register.id">
-        <th>
-          Valor: {{register.value}}
-        </th>
+
+    <!-- <table>
+      <tr>
+        <th>ID</th>
+
+        
+        <th v-for="field in fillableFields" :key="field.id">{{ field.title + ': ' + field.value }}</th>
+
+        <th>Valor</th>
       </tr>
-    </table>
+      
+      <tr v-for="register in registers" :key="register.id">
+        <td>{{register.id}}</td>
 
+        <td v-for="column in register.columns" :key="column.id">{{ column.value_label}}</td>
 
+        <td>{{register.value}}</td>
+      </tr>
+    </table> -->
 
-
-
-
-<!-- 
     <el-table
       v-if="isLoading || hasRegisters"
       v-loading="isLoading"
@@ -59,14 +62,22 @@
       max-height="600"
     >
       <el-table-column label="Código" prop="id" sortable></el-table-column>
-      <el-table-column label="Nombre" prop="title" sortable></el-table-column>
-      <el-table-column
-        label="Descripción"
-        prop="description"
-        min-width="200"
-      ></el-table-column>
 
-      <el-table-column align="right" min-width="200">
+      <el-table-column v-for="field in fillableFields" 
+                         :key="field.id"
+                         :label="field.title + ': ' + field.value"
+                         sortable
+                         >
+
+        <template #default="scope">
+            {{ getColumnValue(scope.row, field) }}
+        </template>
+
+      </el-table-column>
+
+      <el-table-column label="Valor" prop="value" sortable></el-table-column>
+
+      <el-table-column align="right" min-width="150">
         <template #header>
           <el-input
             v-model="search"
@@ -76,41 +87,34 @@
         </template>
         <template #default="scope">
           <el-button
-            type="success"
-            plain
-            @click="$router.push('/registers/' + scope.row.id + '/registers')"
-            >Ver registros</el-button
-          >
-          <el-button
-            type="info"
-            plain
-            @click="$router.push('/registers/' + scope.row.id)"
-            >Ver detalles</el-button
-          >
-
-          <el-button
             type="primary"
             icon="el-icon-edit"
             plain
-            @click="$router.push('/registers/' + scope.row.id + '/edit')"
+            @click="$router.push('/fields/' + scope.row.id + '/edit')"
             >Modificar</el-button
           >
         </template>
       </el-table-column>
+
+
     </el-table>
 
-    <el-empty v-else description="No se ha encontrado ningún Registro"></el-empty> -->
+    <el-empty v-else description="No se ha encontrado ningún registro"></el-empty>
   </el-card>
 </template>
 
 <script>
 import useNotify from "@/hooks/notify.js";
+
+// Components
 import RegisterForm from "@/components/registers/RegisterForm.vue";
+import KpiItem from '@/components/kpis/KpiItem.vue';
 
 export default {
   props: ['id'],
   components: {
-    RegisterForm
+    RegisterForm,
+    KpiItem
   },
   setup() {
     const { notify } = useNotify();
@@ -128,11 +132,14 @@ export default {
   },
   computed: {
     registers() {
-      const belongsToKpi = (register) => {
-        return register.kpiId == this.selectedKpi.id;
-      };
+      if(this.selectedKpi){
+        const belongsToKpi = (register) => {
+          return register.kpiId == this.selectedKpi.id;
+        };
 
-      return this.$store.getters["registers/registers"].filter(belongsToKpi);
+        return this.$store.getters["registers/registers"].filter(belongsToKpi);
+      }
+      return [];
     },
     hasRegisters() {
       return !this.isLoading && this.registers.length > 0;
@@ -146,6 +153,7 @@ export default {
         await this.$store.dispatch("registers/loadRegisters", {
           forceRefresh: refresh,
         });
+        
         if (refresh) {
           this.notify(
             "Registros actualizados",
@@ -170,6 +178,19 @@ export default {
     saveData(data) {
       this.$store.dispatch("registers/registerRegister", data);
     },
+
+    getColumnValue(register, field){
+      var columnValue = null;
+
+      for (const column of register.columns) {
+        if(column.label == field.value) {
+          columnValue = column.value_label;
+          break;
+        }
+      }
+
+      return columnValue;
+    }
   },
   created() {
     this.loadRegisters();
@@ -182,8 +203,13 @@ export default {
     selectedKpi: function(newValue){
       if(newValue){
         this.fillableFields = newValue.fields.filter((field) => field.fillable);
+        console.log(this.fillableFields);
       }
     },
+
+    registers: function(newValue){
+      console.log('Registros:', newValue)
+    }
   },
 };
 </script>
