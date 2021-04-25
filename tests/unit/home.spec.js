b6d37c64dcd53ceb65888f19e2ed466adfd62648
Rubén Rüger <ruben_27895@hotmail.com>
2021-04-25 20:33:24 +0200

    Fallo a proposito para comprobar workflow

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -12,7 +12,7 @@ describe('Home.vue', () => {
     });
 
     // Assert
-    expect(wrapper.text()).toMatch('KPI');
+    expect(wrapper.text()).toMatch('KPIsssssss');
   });
 
   it('Contains cards to travel through app views', () => {
