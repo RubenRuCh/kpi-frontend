9ac3de480017fb0248d7e20c7933c4c7f65aeab0
Rubén Rüger <ruben_27895@hotmail.com>
2021-05-12 19:38:52 +0200

    E2E test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,6 +1,6 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Integration test', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
     cy.get('div[class="logo"]').find('img').should('be.visible');
